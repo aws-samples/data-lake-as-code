eeb082fab2a362a38fb6a2e469a95dffa6ce522b
EC2 Default User <ec2-user@ip-172-31-79-200.ec2.internal>
2020-08-13 18:50:16 +0000

    Moving bindingdb baseline to its own file.

--- lib/baseline-stack.ts
+++ lib/baseline-stack.ts
@@ -9,6 +9,7 @@ import fs = require('fs');
 import { OpenTargetsBaseline } from './baseline-stacks/baseline-opentargets';
 import { ChemblBaseline } from './baseline-stacks/baseline-chembl';
 import { GTExBaseline } from './baseline-stacks/baseline-gtex';
+import { BindingDBBaseline } from './baseline-stacks/baseline-bindingdb';
 
 
 
@@ -134,116 +135,124 @@ export class BaselineStack extends cdk.Stack {
         this.GTExSourceBucket = gtexBaseline.GTExSourceBucket;
 
         //// Start Binding DB  ////
-        
-        const bindingDbAccessSg = new ec2.SecurityGroup(this, 'bindingDbAccessSg', {
-            vpc: baselineVpc,
-            allowAllOutbound: true,
-            description: "Grants access to the BindingDB rds instance",
-            securityGroupName: "BindingDBAccessSecurityGroup"
+  
+        const bindingDbBaseline = new BindingDBBaseline(this, 'bindingDbBaseline',{
+            ImportInstance: importInstanceGtexBindingDb,
+            TargetVPC: baselineVpc
         });
+        this.BindingDb = bindingDbBaseline.BindingDBDatabaseInstance;
+        this.BindingDBAccessSg = bindingDbBaseline.DbAccessSg;
+        this.BindingDBSecret = bindingDbBaseline.DbSecret;
+        
+//         const bindingDbAccessSg = new ec2.SecurityGroup(this, 'bindingDbAccessSg', {
+//             vpc: baselineVpc,
+//             allowAllOutbound: true,
+//             description: "Grants access to the BindingDB rds instance",
+//             securityGroupName: "BindingDBAccessSecurityGroup"
+//         });
         
         
         
-        this.BindingDBSourceBucket = new s3.Bucket(this, 'BindingDbSourceBucket');
+//         this.BindingDBSourceBucket = new s3.Bucket(this, 'BindingDbSourceBucket');
         
-        this.BindingDBAccessSg = bindingDbAccessSg;
+//         this.BindingDBAccessSg = bindingDbAccessSg;
         
-        const bindingDbSg = new ec2.SecurityGroup(this, 'bindingDbSg', {
-            vpc: baselineVpc,
-            allowAllOutbound: true,
-            description: "Security group for binding dbs",
-            securityGroupName: "BindingDbSecurityGroup"
-        });
+//         const bindingDbSg = new ec2.SecurityGroup(this, 'bindingDbSg', {
+//             vpc: baselineVpc,
+//             allowAllOutbound: true,
+//             description: "Security group for binding dbs",
+//             securityGroupName: "BindingDbSecurityGroup"
+//         });
         
         
 
         
-        bindingDbAccessSg.addIngressRule( bindingDbAccessSg , ec2.Port.allTraffic(),  "Recursive SG rule for Glue" );
+//         bindingDbAccessSg.addIngressRule( bindingDbAccessSg , ec2.Port.allTraffic(),  "Recursive SG rule for Glue" );
         
-        bindingDbSg.addIngressRule( bindingDbAccessSg , ec2.Port.tcp(1512),  "Gives BindingDB access security group access to oracle port" );
+//         bindingDbSg.addIngressRule( bindingDbAccessSg , ec2.Port.tcp(1512),  "Gives BindingDB access security group access to oracle port" );
         
-        importInstance.addSecurityGroup(bindingDbAccessSg);
-        
-        
-        const bindingDBSecret = new rds.DatabaseSecret(this, 'bindingDbSecret', {
-            username: 'master',
-        });
-        this.BindingDBSecret = bindingDBSecret;
-        
-        bindingDBSecret.grantRead(importInstanceRole);
-        this.BindingDBSourceBucket.grantReadWrite(importInstanceRole);
+//         importInstance.addSecurityGroup(bindingDbAccessSg);
         
         
-        const bindingDbOptionGroup = new rds.OptionGroup(this, 'bindingDbRdsOptionGroup',{
-            engine: rds.DatabaseInstanceEngine.oracleSe2({
-                version: rds.OracleEngineVersion.VER_19, // different version class for each engine type
-            }),
-            description: "Binding DB Option Group",
-            configurations: [{
-                name: "S3_INTEGRATION",
-                version: "1.0"
-            }],
-        });
+//         const bindingDBSecret = new rds.DatabaseSecret(this, 'bindingDbSecret', {
+//             username: 'master',
+//         });
+//         this.BindingDBSecret = bindingDBSecret;
         
-        const bindingDb = new rds.DatabaseInstance(this, 'bindingDb', {
-            engine: rds.DatabaseInstanceEngine.ORACLE_SE2,
-            masterUsername: 'master',
-            licenseModel: rds.LicenseModel.BRING_YOUR_OWN_LICENSE,
-            vpc: baselineVpc,
-            vpcPlacement: appSubnetSelection, 
-            optionGroup: bindingDbOptionGroup,
-            instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.SMALL),
-            instanceIdentifier: 'binding-db',
-            masterUserPassword: bindingDBSecret.secretValueFromJson('password'),
-            securityGroups: [bindingDbSg, bindingDbAccessSg],
-            deletionProtection: false,
-        });
-        this.BindingDb = bindingDb;
-        
-        var bindingDbCfnDb = this.BindingDb.node.defaultChild as rds.CfnDBInstance;
-        
-        const bindingDbRdsImportRole = new iam.Role(this, 'BindingDbRdsInstanceRole', {
-            assumedBy: new iam.ServicePrincipal('rds.amazonaws.com')
-        });
-        this.BindingDBSourceBucket.grantReadWrite(bindingDbRdsImportRole);
-        
-        bindingDbCfnDb.associatedRoles = [{
-           featureName: "S3_INTEGRATION",
-           roleArn: bindingDbRdsImportRole.roleArn
-        }];
+//         bindingDBSecret.grantRead(importInstanceRole);
+//         this.BindingDBSourceBucket.grantReadWrite(importInstanceRole);
         
         
-        const loadBindingDbDoc = new ssm.CfnDocument(this, 'loadBindingDbDoc', {
-            content: JSON.parse(fs.readFileSync('scripts/ssmdoc.importbindingdb.json', { encoding: 'utf-8' })),
-            documentType: "Command"
-        });
+//         const bindingDbOptionGroup = new rds.OptionGroup(this, 'bindingDbRdsOptionGroup',{
+//             engine: rds.DatabaseInstanceEngine.oracleSe2({
+//                 version: rds.OracleEngineVersion.VER_19, // different version class for each engine type
+//             }),
+//             description: "Binding DB Option Group",
+//             configurations: [{
+//                 name: "S3_INTEGRATION",
+//                 version: "1.0"
+//             }],
+//         });
+        
+//         const bindingDb = new rds.DatabaseInstance(this, 'bindingDb', {
+//             engine: rds.DatabaseInstanceEngine.ORACLE_SE2,
+//             masterUsername: 'master',
+//             licenseModel: rds.LicenseModel.BRING_YOUR_OWN_LICENSE,
+//             vpc: baselineVpc,
+//             vpcPlacement: appSubnetSelection, 
+//             optionGroup: bindingDbOptionGroup,
+//             instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.SMALL),
+//             instanceIdentifier: 'binding-db',
+//             masterUserPassword: bindingDBSecret.secretValueFromJson('password'),
+//             securityGroups: [bindingDbSg, bindingDbAccessSg],
+//             deletionProtection: false,
+//         });
+//         this.BindingDb = bindingDb;
+        
+//         var bindingDbCfnDb = this.BindingDb.node.defaultChild as rds.CfnDBInstance;
+        
+//         const bindingDbRdsImportRole = new iam.Role(this, 'BindingDbRdsInstanceRole', {
+//             assumedBy: new iam.ServicePrincipal('rds.amazonaws.com')
+//         });
+//         this.BindingDBSourceBucket.grantReadWrite(bindingDbRdsImportRole);
+        
+//         bindingDbCfnDb.associatedRoles = [{
+//           featureName: "S3_INTEGRATION",
+//           roleArn: bindingDbRdsImportRole.roleArn
+//         }];
+        
+        
+//         const loadBindingDbDoc = new ssm.CfnDocument(this, 'loadBindingDbDoc', {
+//             content: JSON.parse(fs.readFileSync('scripts/ssmdoc.importbindingdb.json', { encoding: 'utf-8' })),
+//             documentType: "Command"
+//         });
 
 
-        const instantClientBasic = new s3assets.Asset(this, `instantClientBasicRpm`, {
-			path: "baseline_binaries/oracle-instantclient19.8-basic-19.8.0.0.0-1.x86_64.rpm"
-		});
-		instantClientBasic.grantRead(importInstanceRole);
-        const instantClientSqlPlus = new s3assets.Asset(this, `instantClientSqlPlusRpm`, {
-			path: "baseline_binaries/oracle-instantclient19.8-sqlplus-19.8.0.0.0-1.x86_64.rpm"
-		});
-		instantClientSqlPlus.grantRead(importInstanceRole);
+//         const instantClientBasic = new s3assets.Asset(this, `instantClientBasicRpm`, {
+// 			path: "baseline_binaries/oracle-instantclient19.8-basic-19.8.0.0.0-1.x86_64.rpm"
+// 		});
+// 		instantClientBasic.grantRead(importInstanceRole);
+//         const instantClientSqlPlus = new s3assets.Asset(this, `instantClientSqlPlusRpm`, {
+// 			path: "baseline_binaries/oracle-instantclient19.8-sqlplus-19.8.0.0.0-1.x86_64.rpm"
+// 		});
+// 		instantClientSqlPlus.grantRead(importInstanceRole);
         
         
-        const loadBindingDbAssociation = new ssm.CfnAssociation(this, 'loadBindingDbAssociation',{
-            name: loadBindingDbDoc.ref,
-            targets: [
-                { key: "InstanceIds", values: [importInstance.instanceId] }
-            ]
-        });
+//         const loadBindingDbAssociation = new ssm.CfnAssociation(this, 'loadBindingDbAssociation',{
+//             name: loadBindingDbDoc.ref,
+//             targets: [
+//                 { key: "InstanceIds", values: [importInstance.instanceId] }
+//             ]
+//         });
         
-        loadBindingDbAssociation.addPropertyOverride('Parameters',{
-            databaseSecretArn: [this.BindingDBSecret.secretArn],
-            databaseHostName: [this.BindingDb.dbInstanceEndpointAddress],
-            databaseDmpS3Location: [this.BindingDBSourceBucket.bucketName],
-            instantClientBasicS3Path: [instantClientBasic.s3ObjectUrl],
-            instantClientSqlPlusS3Path: [instantClientSqlPlus.s3ObjectUrl],
-            executionTimeout: ['7200']
-        });
+//         loadBindingDbAssociation.addPropertyOverride('Parameters',{
+//             databaseSecretArn: [this.BindingDBSecret.secretArn],
+//             databaseHostName: [this.BindingDb.dbInstanceEndpointAddress],
+//             databaseDmpS3Location: [this.BindingDBSourceBucket.bucketName],
+//             instantClientBasicS3Path: [instantClientBasic.s3ObjectUrl],
+//             instantClientSqlPlusS3Path: [instantClientSqlPlus.s3ObjectUrl],
+//             executionTimeout: ['7200']
+//         });
         
         //// End Binding DB  ////
         
---
+++ lib/baseline-stacks/baseline-bindingdb.ts
@@ -0,0 +1,145 @@
+import * as cdk from '@aws-cdk/core';
+import ec2 = require('@aws-cdk/aws-ec2');
+import iam = require('@aws-cdk/aws-iam');
+import rds = require('@aws-cdk/aws-rds');
+import ssm = require('@aws-cdk/aws-ssm');
+import s3 = require('@aws-cdk/aws-s3');
+import s3assets = require('@aws-cdk/aws-s3-assets');
+import fs = require('fs');
+
+
+
+
+export interface BindingDBBaselineProps extends cdk.StackProps {
+  TargetVPC: ec2.Vpc;
+  ImportInstance: ec2.Instance;
+}
+
+
+export class BindingDBBaseline extends cdk.Construct {
+
+    public readonly DbAccessSg: ec2.SecurityGroup; 
+    public readonly DbSecret: rds.DatabaseSecret;
+    public readonly BindingDBDatabaseInstance: rds.DatabaseInstance;
+    	
+	constructor(scope: cdk.Construct, id: string, props: BindingDBBaselineProps) {
+		super(scope, id);
+    
+        const bindingDBSourceBucket = new s3.Bucket(this, 'BindingDbSourceBucket');
+        
+        this.DbAccessSg = new ec2.SecurityGroup(this, 'SecGroupChemblAccess', {
+            vpc: props.TargetVPC,
+            allowAllOutbound: true,
+            description: "Grants access to the BindingDb rds instances",
+            securityGroupName: "BindingDBAccessSG"
+        });
+        
+        
+        const DbSG = new ec2.SecurityGroup(this, 'SecGroupBindingDbSecGroup', {
+            vpc: props.TargetVPC,
+            allowAllOutbound: true,
+            description: "Security group for BindingDB dbs",
+            securityGroupName: "BindingDbSG"
+        });
+        
+        const dmzSubnetSelection = { subnetType: ec2.SubnetType.PUBLIC };
+        const appSubnetSelection = { subnetType: ec2.SubnetType.PRIVATE };
+        const dbSubnetSelection = { subnetType: ec2.SubnetType.ISOLATED };
+        
+        this.DbAccessSg.addIngressRule( this.DbAccessSg , ec2.Port.allTraffic(),  "Recursive SG rule for Glue" );
+        DbSG.addIngressRule( this.DbAccessSg , ec2.Port.tcp(1512),  "Gives BindingDB access security group access to postgres port" );
+        
+        props.ImportInstance.addSecurityGroup(this.DbAccessSg);
+        
+        
+        
+        
+        const appSubnets = props.TargetVPC.selectSubnets(appSubnetSelection);
+        
+        const DbSecret = new rds.DatabaseSecret(this, 'bindingDbSecret', {
+            username: 'master',
+        });
+        this.DbSecret = DbSecret;
+        
+        this.DbSecret.grantRead(props.ImportInstance.role);
+        
+        bindingDBSourceBucket.grantReadWrite(props.ImportInstance.role);
+        
+        const bindingDbOptionGroup = new rds.OptionGroup(this, 'bindingDbRdsOptionGroup',{
+            engine: rds.DatabaseInstanceEngine.oracleSe2({
+                version: rds.OracleEngineVersion.VER_19, // different version class for each engine type
+            }),
+            description: "Binding DB Option Group",
+            configurations: [{
+                name: "S3_INTEGRATION",
+                version: "1.0"
+            }],
+        });
+        
+        const bindingDb = new rds.DatabaseInstance(this, 'bindingDb', {
+            engine: rds.DatabaseInstanceEngine.ORACLE_SE2,
+            masterUsername: 'master',
+            licenseModel: rds.LicenseModel.BRING_YOUR_OWN_LICENSE,
+            vpc: props.TargetVPC,
+            vpcPlacement: appSubnetSelection, 
+            optionGroup: bindingDbOptionGroup,
+            instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.SMALL),
+            instanceIdentifier: 'binding-db',
+            masterUserPassword: this.DbSecret.secretValueFromJson('password'),
+            securityGroups: [this.DbAccessSg, DbSG],
+            deletionProtection: false,
+        });
+        this.BindingDBDatabaseInstance = bindingDb;
+        
+        
+        
+        var bindingDbCfnDb = this.BindingDBDatabaseInstance.node.defaultChild as rds.CfnDBInstance;
+        
+        const bindingDbRdsImportRole = new iam.Role(this, 'BindingDbRdsInstanceRole', {
+            assumedBy: new iam.ServicePrincipal('rds.amazonaws.com')
+        });
+        bindingDBSourceBucket.grantReadWrite(bindingDbRdsImportRole);
+        
+        bindingDbCfnDb.associatedRoles = [{
+           featureName: "S3_INTEGRATION",
+           roleArn: bindingDbRdsImportRole.roleArn
+        }];
+        
+        
+        const loadBindingDbDoc = new ssm.CfnDocument(this, 'loadBindingDbDoc', {
+            content: JSON.parse(fs.readFileSync('scripts/ssmdoc.importbindingdb.json', { encoding: 'utf-8' })),
+            documentType: "Command"
+        });
+
+
+        const instantClientBasic = new s3assets.Asset(this, `instantClientBasicRpm`, {
+			path: "baseline_binaries/oracle-instantclient19.8-basic-19.8.0.0.0-1.x86_64.rpm"
+		});
+		instantClientBasic.grantRead(props.ImportInstance.role);
+        const instantClientSqlPlus = new s3assets.Asset(this, `instantClientSqlPlusRpm`, {
+			path: "baseline_binaries/oracle-instantclient19.8-sqlplus-19.8.0.0.0-1.x86_64.rpm"
+		});
+		instantClientSqlPlus.grantRead(props.ImportInstance.role);
+        
+        
+        const loadBindingDbAssociation = new ssm.CfnAssociation(this, 'loadBindingDbAssociation',{
+            name: loadBindingDbDoc.ref,
+            targets: [
+                { key: "InstanceIds", values: [props.ImportInstance.instanceId] }
+            ]
+        });
+        
+        loadBindingDbAssociation.addPropertyOverride('Parameters',{
+            databaseSecretArn: [this.DbSecret.secretArn],
+            databaseHostName: [this.BindingDBDatabaseInstance.dbInstanceEndpointAddress],
+            databaseDmpS3Location: [bindingDBSourceBucket.bucketName],
+            instantClientBasicS3Path: [instantClientBasic.s3ObjectUrl],
+            instantClientSqlPlusS3Path: [instantClientSqlPlus.s3ObjectUrl],
+            executionTimeout: ['7200']
+        });
+        
+        
+    }
+    
+    
+}
