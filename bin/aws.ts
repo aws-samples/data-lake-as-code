639711860684d280bbe792cee709db2cb8ba404c
EC2 Default User <ec2-user@ip-172-31-79-200.ec2.internal>
2020-08-26 20:33:03 +0000

    Commenting out example code.

--- bin/aws.ts
+++ bin/aws.ts
@@ -18,34 +18,36 @@ const coreDataLake = new DataLakeStack(app, 'CoreDataLake', {
 });
 
 
-const exampleS3DataSet = new ExampleS3DataSet(app, 'exampleS3DataSet', {
-    sourceBucket: s3.Bucket.fromBucketName(app, 'exampleS3DataSetSourceBucket', '--- YOUR EXISTING BUCKET NAME GOES HERE ---'),
-    sourceBucketDataPrefix: '/prefixToParentFolderofTableFolders/',
-    DataLake: coreDataLake
-});
+// const exampleS3DataSet = new ExampleS3DataSet(app, 'exampleS3DataSet', {
+//     sourceBucket: s3.Bucket.fromBucketName(app, 'exampleS3DataSetSourceBucket', '--- YOUR EXISTING BUCKET NAME GOES HERE ---'),
+//     sourceBucketDataPrefix: '/prefixToParentFolderofTableFolders/',
+//     DataLake: coreDataLake
+// });
 
 
-const examplePgRdsDataSet = new ExamplePgRdsDataSet(app, 'exampleS3DataSet', {
+// const examplePgRdsDataSet = new ExamplePgRdsDataSet(app, 'examplePgRdsDataSet', {
     
-    database: rds.DatabaseInstance.fromDatabaseInstanceAttributes(app, 'exampleRdsDataSet', {
-        instanceEndpointAddress: '--- RDS INSTANCE ENDPOINT ADDRESS GOES HERE ---',
-        instanceIdentifier: '--- RDS INSTANCE IDENTIFIRE GOES HERE ---',
-        port: 5432,
-        securityGroups: []}) as rds.DatabaseInstance,
-    databaseSecret: rds.DatabaseSecret.fromSecretArn(app, 'exampleRdsDataSetSecret', 
-        '---SECRET ARN GOES HERE ---') as rds.DatabaseSecret,
-    accessSecurityGroup: ec2.SecurityGroup.fromSecurityGroupId(app, 'exampleRdsAccessSecurityGroup',
-        '---SECURITY GROUP ID THAT ALLOWS INBOUND ACCESS TO DATABASE GOES HERE ---') as ec2.SecurityGroup,
-    DataLake: coreDataLake    
-});
+//     database: rds.DatabaseInstance.fromDatabaseInstanceAttributes(app, 'exampleRdsDataSet', {
+//         instanceEndpointAddress: '--- RDS INSTANCE ENDPOINT ADDRESS GOES HERE ---',
+//         instanceIdentifier: '--- RDS INSTANCE IDENTIFIRE GOES HERE ---',
+//         port: 5432,
+//         securityGroups: []}) as rds.DatabaseInstance,
+//     databaseSecret: rds.DatabaseSecret.fromSecretArn(app, 'exampleRdsDataSetSecret', 
+//         '---SECRET ARN GOES HERE ---') as rds.DatabaseSecret,
+//     accessSecurityGroup: ec2.SecurityGroup.fromSecurityGroupId(app, 'exampleRdsAccessSecurityGroup',
+//         '---SECURITY GROUP ID THAT ALLOWS INBOUND ACCESS TO DATABASE GOES HERE ---') as ec2.SecurityGroup,
+//     DataLake: coreDataLake    
+// });
 
 
+// Grant permissions:
 
+// const exampleExistingIamUser = iam.User.fromUserName(coreDataLake, 'exampleUserGrantee', '--- EXISTING IAM USERNAME GOES HERE --' );
+// const exampleExistingIamRole = iam.Role.fromRoleArn(coreDataLake, 'exampleRoleGrantee', '--- EXISTING IAM ROLE ARN GOES HERE --' );
 
+// exampleS3DataSet.grantIamRead(exampleExistingIamUser);
+// exampleS3DataSet.grantIamRead(exampleExistingIamRole);
 
-// chemblStack.grantIamRead(analyticsStack.NotebookRole);
-
-// const exampleUser = iam.User.fromUserName(coreDataLake, 'exampleGrantee', 'paul1' );
 
 // var exampleGrant: DataLakeEnrollment.TablePermissionGrant = {
 //     tables: ["association_data", "evidence_data","target_list","disease_list"],
@@ -55,33 +57,5 @@ const examplePgRdsDataSet = new ExamplePgRdsDataSet(app, 'exampleS3DataSet', {
 //     GrantableTablePermissions: [DataLakeEnrollment.TablePermission.Select]
 // };
 
-// openTargetsStack.grantTablePermissions(exampleUser, exampleGrant);
-
-
-
-// // In the example below, we are using the compound_structures table from ChEMBL. It has the following table definition:
-// // ['molregno', 'molfile', 'standard_inchi', 'standard_inchi_key', 'canonical_smiles']
-// // Lets say we want to give a principal ONLY select permissions to everything in the compound_structures table BUT the 'canonical_smiles' column.
-
-// var exampleTableWithColumnsGrant: DataLakeEnrollment.TableWithColumnPermissionGrant = {
-//     table: "chembl_25_public_compound_structures",
-//     // Note that we are NOT including 'canonical_smiles'. That effectivley prevents this user from querying that column.
-//     columns: ['molregno', 'molfile', 'standard_inchi', 'standard_inchi_key'],
-//     DatabasePermissions: [],
-//     GrantableDatabasePermissions: [],
-//     TableColumnPermissions: [DataLakeEnrollment.TablePermission.Select],
-//     GrantableTableColumnPermissions: []
-// };
-
-// var exampleTableWithColumnsGrant_WithWildCard: DataLakeEnrollment.TableWithColumnPermissionGrant = {
-//     table: "chembl_25_public_compound_structures",
-//     wildCardFilter: DataLakeEnrollment.TableWithColumnFilter.Exclude,
-//     columns: ['canonical_smiles'],
-//     DatabasePermissions: [],
-//     GrantableDatabasePermissions: [],
-//     TableColumnPermissions: [DataLakeEnrollment.TablePermission.Select],
-//     GrantableTableColumnPermissions: []
-// };
-
-// // Note that exampleTableWithColumnsGrant exampleTableWithColumnsGrant_WithWildCard grants the same effecitve permissions. One just uses a the wildcard.
-// chemblStack.grantTableWithColumnPermissions(exampleUser, exampleTableWithColumnsGrant);
+// examplePgRdsDataSet.grantTablePermissions(exampleExistingIamUser, exampleGrant);
+// examplePgRdsDataSet.grantTablePermissions(exampleExistingIamRole, exampleGrant);
