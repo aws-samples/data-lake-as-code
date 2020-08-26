3233fbe77e00c1e7af05eacc5b0902a5bc1f63eb
EC2 Default User <ec2-user@ip-172-31-79-200.ec2.internal>
2020-08-26 22:02:01 +0000

    Refactoring variable

--- lib/ExampleS3DataSet-stack.ts
+++ lib/ExampleS3DataSet-stack.ts
@@ -19,10 +19,10 @@ export class ExampleS3DataSet extends DataSetStack{
 	constructor(scope: cdk.Construct, id: string, props: ExampleS3DataSetProps) {
 	    super(scope, id, props);
 	    
+        const dataSetName = "exampledataset_v1"
 
-
-	    this.Enrollments.push(new S3dataSetEnrollment(this, 'exampledataset-v1-enrollment', {
-	        DataSetName: "exampledataset_v1",
+	    this.Enrollments.push(new S3dataSetEnrollment(this, `${dataSetName}Enrollment`, {
+	        DataSetName: dataSetName,
 	        sourceBucket: props.sourceBucket,
 	        MaxDPUs: 3.0,
             sourceBucketDataPrefixes: [
@@ -39,36 +39,13 @@ export class ExampleS3DataSet extends DataSetStack{
                 "--enable-metrics": "",
                 "--DL_BUCKET": props.DataLake.DataLakeBucket.bucketName,
                 "--DL_REGION": cdk.Stack.of(this).region,
-                "--DL_PREFIX": "/exampledataset_v1/",
-                "--GLUE_SRC_DATABASE": "exampledataset_v1_src"
+                "--DL_PREFIX": `/${dataSetName}/`,
+                "--GLUE_SRC_DATABASE": `${dataSetName}_src`
             }
 	    }));
 
 
 
-	   // this.Enrollments.push(new S3dataSetEnrollment(this, 'openTargets-1911-enrollment', {
-	   //     DataSetName: "exampledataset_v2",
-	   //     sourceBucket: props.sourceBucket,
-	   //     MaxDPUs: 3.0,
-    //         sourceBucketDataPrefixes: [
-    //             `${props.sourceBucketDataPrefix}19.11/output/19.11_association_data/` ,
-    //             `${props.sourceBucketDataPrefix}19.11/output/19.11_disease_list/`,
-    //             `${props.sourceBucketDataPrefix}19.11/output/19.11_evidence_data/`, 
-    //             `${props.sourceBucketDataPrefix}19.11/output/19.11_target_list/`
-    //         ],
-	   //     dataLakeBucket: props.DataLake.DataLakeBucket,
-	   //     GlueScriptPath: "scripts/glue.s3import.exampledataset.v1.py",
-	   //     GlueScriptArguments: {
-    //             "--job-language": "python", 
-    //             "--job-bookmark-option": "job-bookmark-disable",
-    //             "--enable-metrics": "",
-    //             "--DL_BUCKET": props.DataLake.DataLakeBucket.bucketName,
-    //             "--DL_REGION": cdk.Stack.of(this).region,
-    //             "--DL_PREFIX": "/exampledataset_v2/",
-    //             "--GLUE_SRC_DATABASE": "exampledataset_v2_src"
-    //         }
-	   // }));
-	    
 	    
 	}
 }
