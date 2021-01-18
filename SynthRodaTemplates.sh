d55d95e65acc7efdd151f3480f56e11e49488449
Andrew de Rozario <andrew.de.rozario@and.digital>
2021-01-18 17:12:50 +0000

    updating Roda table and database names for template synth script

--- SynthRodaTemplates.sh
+++ SynthRodaTemplates.sh
@@ -1,16 +1,16 @@
-aws glue get-tables --database-name opentargets_1911_dl > RODA_templates/open_targets_1911_get_tables.json
-aws glue get-database --name opentargets_1911_dl > RODA_templates/open_targets_1911_get_database.json
+aws glue get-tables --database-name opentargets_1911_dl-awsroda > RODA_templates/open_targets_1911_get_tables.json
+aws glue get-database --name opentargets_1911_dl-awsroda > RODA_templates/open_targets_1911_get_database.json
 npm run build && cdk synth OpenTargetsRodaTemplate
 aws s3 cp cdk.out/OpenTargetsRodaTemplate.template.json s3://aws-roda-hcls-datalake/OpenTargetsRodaTemplate.json
 
 
-aws glue get-tables --database-name chembl_25_dl > RODA_templates/chembl_25_get_tables.json
-aws glue get-database --name chembl_25_dl > RODA_templates/chembl_25_get_database.json
+aws glue get-tables --database-name chembl_25_dl-awsroda > RODA_templates/chembl_25_get_tables.json
+aws glue get-database --name chembl_25_dl-awsroda > RODA_templates/chembl_25_get_database.json
 npm run build && cdk synth ChemblRodaTemplate
 aws s3 cp cdk.out/ChemblRodaTemplate.template.json s3://aws-roda-hcls-datalake/ChemblRodaTemplate.json
 
-aws glue get-tables --database-name binding_db_dl > RODA_templates/binding_db_get_tables.json
-aws glue get-database --name binding_db_dl > RODA_templates/binding_db_get_database.json
+aws glue get-tables --database-name binding_db_dl-awsroda > RODA_templates/binding_db_get_tables.json
+aws glue get-database --name binding_db_dl-awsroda > RODA_templates/binding_db_get_database.json
 npm run build && cdk synth BindingDbRodaTemplate
 aws s3 cp cdk.out/BindingDbRodaTemplate.template.json s3://aws-roda-hcls-datalake/BindingDbRodaTemplate.json
 
